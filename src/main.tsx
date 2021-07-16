a6b45a40a08c35c0efc112a02162be44691dcb94
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-16 17:49:57 -0300

    config(deps): set up tailwind, typescript, prettier, eslint, vite

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <h1> Hello</h1>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontWeight: ['hover', 'focus'],
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
