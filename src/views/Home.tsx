6a432092dad98c0ef06a91a9ec12be1906141287
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-17 16:10:06 -0300

    wip(home): add debounce input

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,6 +3,8 @@ import { WithPeopleList } from '../components/WithPeopleList';
 import { PeopleList } from '../components/PeopleList';
 import { SearchIcon } from '@heroicons/react/solid';
 
+import { DebounceInput } from 'react-debounce-input';
+
 const Home = () => {
   const [search, setSearch] = React.useState('');
 
@@ -21,21 +23,20 @@ const Home = () => {
           <div className="pointer-events-none absolute inset-y-0 right-0 pr-3 flex items-center">
             <SearchIcon className="h-5 w-5" aria-hidden="true" />
           </div>
-          <input
-            id="search"
+          <DebounceInput
             className="block w-full bg-white py-2 pl-6 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 ring-2 ring-offset-2 ring-offset-secondary  ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:text-sm"
-            placeholder="Searching"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            type="search"
+            id="search"
             name="search"
+            type="search"
+            placeholder="Searching"
+            minLength={2}
+            debounceTimeout={300}
+            onChange={(event) => setSearch(event.target.value)}
           />
         </div>
       </div>
       <WithPeopleList>
-        {(query) => (
-          <PeopleList search={search} query={query} />
-        )}
+        {(query) => <PeopleList search={search} query={query} />}
       </WithPeopleList>
     </React.Fragment>
   );
