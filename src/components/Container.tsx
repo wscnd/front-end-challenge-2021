e6e6ca88948c7423d0d160df5ae42327ee3a783e
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-17 20:58:53 -0300

    fix(home): hack to have consistent viewport size width and height

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Container: React.FunctionComponent<React.ReactNode> = (props) => {
   return props.children ? (
-    <div className="bg-bg min-h-screen">{props.children}</div>
+    <div className="overflow-x-hidden">
+      <div className="bg-bg w-screen min-h-screen">{props.children}</div>
+    </div>
   ) : null;
 };
 
