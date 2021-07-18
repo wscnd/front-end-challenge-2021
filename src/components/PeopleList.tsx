ec47b23232e88038456f0d147a83453d58927010
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-18 00:02:58 -0300

    wip(home): add filter by gender

--- src/components/PeopleList.tsx
+++ src/components/PeopleList.tsx
@@ -1,27 +1,21 @@
 import { PlusIcon, RefreshIcon } from '@heroicons/react/solid';
 import * as React from 'react';
 import { ShowRefreshing } from '../components/ShowRefreshing';
-import type { Person } from '../lib/types/Person';
 import type { UsePersonList } from '../lib/types/PersonQuery';
+import { filterByGender, filterByName } from '../lib/utils/filterPerson';
 import { ShowLoading } from './ShowLoading';
 import { Table, TableBody } from './Table';
 
 type PeopleListProps = React.FunctionComponent<{
   query: UsePersonList;
   search: string;
+  filter: { gender: string };
 }>;
 
-const PeopleList: PeopleListProps = ({ query, search }) => {
-  React.useEffect(() => {
-    console.log(search);
-  });
-
-  const filterPeople = (person: Person) => {
-    if (search) {
-      const fullName = `${person.name.first} ${person.name.last}`;
-      return fullName.toLowerCase().includes(search.toLowerCase());
-    } else return person;
-  };
+const PeopleList: PeopleListProps = ({ query, search, filter }) => {
+  // React.useEffect(() => {
+  //   console.log('filter:', filter);
+  // });
 
   const fetchNextPage = React.useCallback(() => {
     query.fetchNextPage();
@@ -29,7 +23,7 @@ const PeopleList: PeopleListProps = ({ query, search }) => {
 
   return (
     <div className="mb-6">
-      <div className="pointer-events-none fixed bottom-6 right-6 flex items-center">
+      <div className="fixed flex items-center pointer-events-none bottom-6 right-6">
         <ShowRefreshing
           Icon={RefreshIcon}
           show={
@@ -42,7 +36,10 @@ const PeopleList: PeopleListProps = ({ query, search }) => {
           {query.data?.pages.length
             ? query.data?.pages.map((pages, index) => (
                 <TableBody
-                  personList={pages.results.filter(filterPeople)}
+                  personList={pages.results
+                    //NOTE: probably expensive?
+                    .filter((person) => filterByName(person, search))
+                    .filter((person) => filterByGender(person, filter.gender))}
                   key={index}
                 />
               ))
@@ -54,13 +51,13 @@ const PeopleList: PeopleListProps = ({ query, search }) => {
           text={'Loading...'}
         />
       </section>
-      <div className="w-full h-10 flex justify-center">
+      <div className="flex justify-center w-full h-10">
         {!query.isFetchingNextPage && !query.isLoading ? (
           <button
             type="button"
             onClick={fetchNextPage}
             disabled={query.isFetching}
-            className=" inline-flex disabled:bg-bg-2 disabled:cursor-not-allowed disabled:text-gray-400 items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-bg bg-primary hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 border border-transparent rounded-md shadow-sm  disabled:bg-bg-2 disabled:cursor-not-allowed disabled:text-gray-400 text-bg bg-primary hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
           >
             <PlusIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
             Fetch More
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,11 +2,23 @@ import { SearchIcon } from '@heroicons/react/solid';
 import * as React from 'react';
 import { DebounceInput } from 'react-debounce-input';
 import { PeopleList } from '../components/PeopleList';
+import { Select } from '../components/Select';
 import { WithPeopleList } from '../components/WithPeopleList';
-
 
 const Home = () => {
   const [search, setSearch] = React.useState('');
+
+  const options = React.useState(() => [
+    { id: 1, name: 'Male', value: 'male' },
+    { id: 2, name: 'Female', value: 'female' },
+    { id: 3, name: 'Any', value: '' },
+  ])[0];
+
+  const [selected, setSelected] = React.useState(options[2]);
+
+  // React.useEffect(() => {
+  //   console.log(selected);
+  // }, [selected]);
 
   return (
     <React.Fragment>
@@ -18,25 +30,37 @@ const Home = () => {
           neque culpa aut nam?
         </p>
       </section>
-      <section className="pb-12">
-        <div className="relative text-gray-400 focus-within:text-gray-600">
-          <div className="pointer-events-none absolute inset-y-0 right-4 pr-3 flex items-center">
-            <SearchIcon className="h-5 w-5" aria-hidden="true" />
+      <section className="grid grid-cols-4 grid-rows-1 gap-4 mb-8 ">
+        <div className="col-start-1 col-end-4 flex items-end ">
+          <div className="relative text-gray-400 focus-within:text-gray-600  bottom-0 w-full">
+            <div className="pointer-events-none absolute inset-y-0 right-4 pr-3 flex items-center">
+              <SearchIcon className="h-5 w-5" aria-hidden="true" />
+            </div>
+            <DebounceInput
+              className="block w-full bg-white py-2 pl-6 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 ring-2 ring-offset-2 ring-offset-secondary  ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:text-sm"
+              id="search"
+              name="search"
+              type="search"
+              placeholder="Searching"
+              minLength={2}
+              debounceTimeout={300}
+              onChange={(event) => setSearch(event.target.value)}
+            />
           </div>
-          <DebounceInput
-            className="block w-full bg-white py-2 pl-6 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 ring-2 ring-offset-2 ring-offset-secondary  ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:text-sm"
-            id="search"
-            name="search"
-            type="search"
-            placeholder="Searching"
-            minLength={2}
-            debounceTimeout={300}
-            onChange={(event) => setSearch(event.target.value)}
-          />
+        </div>
+
+        <div className="grid-start-4">
+          <Select selectOptions={[selected, setSelected]} options={options} />
         </div>
       </section>
       <WithPeopleList>
-        {(query) => <PeopleList search={search} query={query} />}
+        {(query) => (
+          <PeopleList
+            search={search}
+            query={query}
+            filter={{ gender: selected.value }}
+          />
+        )}
       </WithPeopleList>
     </React.Fragment>
   );
