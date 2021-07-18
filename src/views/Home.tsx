a4dc53ae76ce341de0fcdc061b57fd71798cd070
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-18 19:45:50 -0300

    wip(filter-nationality): get nationalities based on query results and filter persons based on it

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,7 @@
 import { RefreshIcon, SearchIcon } from '@heroicons/react/solid';
 import * as React from 'react';
 import { DebounceInput } from 'react-debounce-input';
+import { useInfiniteQuery } from 'react-query';
 import { useHistory } from 'react-router-dom';
 import { PeopleList } from '../components/PeopleList';
 import { QueryMoreButton } from '../components/QueryMoreButton';
@@ -10,6 +11,8 @@ import { ShowRefreshing } from '../components/ShowRefreshing';
 import { WithRouterPeopleListQuery } from '../components/WithRouterPeopleListQuery';
 import { SortContextProvider } from '../context/SortContext';
 import { useSearchParams } from '../hooks/useSearchParams';
+import { Person } from '../lib/types/Person';
+import { QueryResult } from '../lib/types/QueryResult';
 
 const Home = () => {
   const { page } = useSearchParams('page');
@@ -18,19 +21,57 @@ const Home = () => {
 
   const [search, setSearch] = React.useState('');
 
-  const options = React.useState(() => [
+  const queryClient = useInfiniteQuery<QueryResult>('people');
+
+  // NOTE: Gender options
+  const genderOptions = React.useState(() => [
     { id: 1, name: 'Male', value: 'male' },
     { id: 2, name: 'Female', value: 'female' },
     { id: 3, name: 'Any', value: '' },
   ])[0];
 
-  const [selected, setSelected] = React.useState(options[2]);
+  const [selectedGender, setSelectedGender] = React.useState(genderOptions[2]);
+
+  // NOTE: Nationality options, this is intense and probably overkill
+  const nationalityOptions = React.useMemo(() => {
+    const anyNationality = { name: 'Any', value: '', id: 0 };
+    return Object.values(
+      queryClient.data?.pages
+        .reduce(
+          (store: Person[], current) => [...store, ...current.results],
+          [],
+        )
+        .reduce(
+          (store, person) => ({
+            ...store,
+            [person.nat]: {
+              name: person.nat,
+              value: person.nat,
+              id: person.nat,
+            },
+          }),
+          { any: anyNationality },
+        ) || [anyNationality],
+    )?.map((value, index) => ({ ...value, id: index }));
+  }, [queryClient.data?.pages]);
+
+  const [selectNationality, setSelectedNationality] = React.useState(
+    nationalityOptions[0],
+  );
+
+  React.useEffect(() => {
+    console.log('nationalityOptions:', nationalityOptions);
+  }, [nationalityOptions]);
+
+  React.useEffect(() => {
+    console.log('selectNationality:', selectNationality);
+  }, [selectNationality]);
 
   React.useEffect(() => {
     // const pageFromParam = page ? `page=${page}` : '';
     // console.log('pageFromParam:', pageFromParam);
     // console.log('gender: ', selected);
-  }, [selected, history, page]);
+  }, [selectedGender, history, page]);
 
   return (
     <React.Fragment>
@@ -42,14 +83,15 @@ const Home = () => {
           neque culpa aut nam?
         </p>
       </section>
-      <section className="grid grid-cols-4 grid-rows-1 gap-4 mb-8 ">
+      <section className="grid grid-cols-5 grid-rows-1 gap-6 mb-8 ">
         <div className="flex items-end col-start-1 col-end-4 ">
           <div className="relative bottom-0 w-full text-gray-400 focus-within:text-gray-600">
             <div className="absolute inset-y-0 flex items-center pr-3 pointer-events-none right-4">
               <SearchIcon className="w-5 h-5" aria-hidden="true" />
             </div>
             <DebounceInput
-              className="block w-full py-2 pl-6 pr-3 leading-5 text-gray-900 placeholder-gray-500 bg-white border border-transparent rounded-md ring-2 ring-offset-2 ring-offset-secondary ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:text-sm"
+              maxLength="25"
+              className="block w-full py-2 pl-6 pr-3 leading-5 text-gray-900 placeholder-gray-500 bg-white border border-transparent rounded-md ring-2 ring-offset-2 ring-offset-secondary ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:text-sm "
               id="search"
               name="search"
               type="search"
@@ -61,7 +103,14 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="grid-start-4">
+        <div className="col-start-4">
+          <Select
+            selectOptions={[selectNationality, setSelectedNationality]}
+            options={nationalityOptions}
+            labelText={'Select Nationality'}
+          />
+        </div>
+        <div className="col-start-5">
           <Select
             selectOptions={[selectedGender, setSelectedGender]}
             options={genderOptions}
@@ -89,7 +138,10 @@ const Home = () => {
                     <PeopleList
                       search={search}
                       pages={query.data.pages}
-                      filter={{ gender: selected.value }}
+                      filter={{
+                        gender: selectedGender.value,
+                        nationality: selectNationality.value,
+                      }}
                     />
                   </SortContextProvider>
                 ) : null}
