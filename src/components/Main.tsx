3355f941eaaf9940e5affad82dcde1a12aae4ea9
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-20 16:39:18 -0300

    wip(responsive): make components responsive (media queries)

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const Main: React.FunctionComponent<React.ReactNode> = (props) => {
   return props.children ? (
-    <main className="px-32 py-20 ">
+    <main className="px-3 py-10 sm:px-10 sm:py-10 lg:px-32 lg:py-20">
       <div className="max-w-5xl mx-auto sm:px-6 lg:px-8">{props.children}</div>
     </main>
   ) : null;
--- src/components/SortButton.tsx
+++ src/components/SortButton.tsx
@@ -35,19 +35,19 @@ const SortButton = () => {
   return (
     <button
       type="button"
-      className="absolute right-3 hover:text-gray-500 has-tooltip focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white"
+      className="absolute right-0 md:right-3 hover:text-gray-500 has-tooltip focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white"
       onClick={handleClick}
     >
       <React.Fragment>
-        <p className="ml-5 -mt-5 tooltip rounded shadow-lg text-xs bg-gray-100 text-primary ">
+        <p className="ml-5 -mt-5 text-xs bg-gray-100 rounded shadow-lg tooltip text-primary ">
           sort {setNextOrder[sorting.order]}
         </p>
         {sorting.order === 'ascending' ? (
-          <SortDescendingIcon className=" h-5 w-5 " />
+          <SortDescendingIcon className="w-4 h-4 sm:h-5 sm:w-5 " />
         ) : sorting.order === 'descending' ? (
-          <AdjustmentsIcon className=" h-5 w-5" />
+          <AdjustmentsIcon className="w-4 h-4  sm:h-5 sm:w-5" />
         ) : (
-          <SortAscendingIcon className=" h-5 w-5" />
+          <SortAscendingIcon className="w-4 h-4  sm:h-5 sm:w-5" />
         )}
       </React.Fragment>
     </button>
