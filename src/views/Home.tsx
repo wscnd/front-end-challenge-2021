6226c3c13cffb1c8b0a5aa887bbb37083d331730
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-20 16:39:30 -0300

    wip(responsive): make homepage responsive

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -75,23 +75,23 @@ const Home = () => {
 
   return (
     <React.Fragment>
-      <section className="mb-6">
-        <p className="font-semibold prose-xl text-left text-primary">
+      <section className="mb-6 w-full">
+        <span className="font-semibold  prose text-left lg:prose-xl md:prose-lg text-primary ">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, fuga?
           Ducimus porro velit temporibus qui ea nostrum voluptatibus expedita
           eligendi deserunt pariatur! Voluptates amet distinctio voluptatum
           neque culpa aut nam?
-        </p>
+        </span>
       </section>
-      <section className="grid grid-cols-5 grid-rows-1 gap-6 mb-8 ">
-        <div className="flex items-end col-start-1 col-end-4 ">
+      <section className="grid grid-cols-2 grid-rows-2 mb-8 md:grid-cols-5 md:grid-rows-1  md:gap-6 gap-3">
+        <div className="flex items-end col-start-1 col-end-3  md:col-end-4 ">
           <div className="relative bottom-0 w-full text-gray-400 focus-within:text-gray-600">
             <div className="absolute inset-y-0 flex items-center pr-3 pointer-events-none right-4">
               <SearchIcon className="w-5 h-5" aria-hidden="true" />
             </div>
             <DebounceInput
               maxLength="25"
-              className="block w-full py-2 pl-6 pr-3 leading-5 text-gray-900 placeholder-gray-500 bg-white border border-transparent rounded-md ring-2 ring-offset-2 ring-offset-secondary ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:text-sm "
+              className="block w-full py-3 pl-6 pr-3 leading-5 text-gray-900 placeholder-gray-500 bg-white border border-transparent rounded-md ring-2 ring-offset-2 ring-offset-secondary ring-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white focus:border-white sm:prose-sm lg:prose-lg"
               id="search"
               name="search"
               type="search"
@@ -103,18 +103,18 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="col-start-4">
+        <div className="md:col-start-4 flex flex-col justify-end">
           <Select
             selectOptions={[selectNationality, setSelectedNationality]}
             options={nationalityOptions}
-            labelText={'Select Nationality'}
+            labelText={'Nationality'}
           />
         </div>
-        <div className="col-start-5">
+        <div className="md:col-start-5 flex flex-col justify-end">
           <Select
             selectOptions={[selectedGender, setSelectedGender]}
             options={genderOptions}
-            labelText={'Select Gender'}
+            labelText={'Gender'}
           />
         </div>
       </section>
