2f85eb9cdfba1d3a12ebd53de28a6105d68fd469
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-19 23:23:33 -0300

    wip(view-person): expose View action to show the DisplayPerson modal

--- src/components/PeopleList.tsx
+++ src/components/PeopleList.tsx
@@ -2,8 +2,13 @@ import * as React from 'react';
 import { useSortContext } from '../context/SortContext';
 import type { Person } from '../lib/types/Person';
 import type { PersonPage } from '../lib/types/PersonQuery';
-import { filterByGender, filterByName, filterByNationality } from '../lib/utils/mutatePerson';
+import {
+  filterByGender,
+  filterByName,
+  filterByNationality,
+} from '../lib/utils/mutatePerson';
 import { mutatePersonList as orderPersonList } from '../lib/utils/mutatePersonList';
+import { DisplayPerson } from './DisplayPerson';
 import { Table, TableBody } from './Table';
 
 type PeopleListProps = React.FunctionComponent<{
@@ -15,6 +20,14 @@ type PeopleListProps = React.FunctionComponent<{
 const PeopleList: PeopleListProps = ({ pages, search, filter }) => {
   const sortOrder = useSortContext()[0];
 
+  const [viewPerson, setViewPerson] = React.useState(false);
+  const [selectedPersonInfo, setSelectedPersonInfo] = React.useState<Person>();
+
+  const viewAction = React.useCallback((person: Person) => {
+    setViewPerson((previous) => !previous);
+    setSelectedPersonInfo(person);
+  }, []);
+
   React.useEffect(() => {
     console.log('sort Order', sortOrder);
   }, [sortOrder]);
@@ -24,22 +37,31 @@ const PeopleList: PeopleListProps = ({ pages, search, filter }) => {
   }, [filter]);
 
   return (
-    <Table>
-      <TableBody
-        //NOTE: probably expensive?
-        personList={orderPersonList(
-          pages.reduce(
-            (store: Person[], current) => [...store, ...current.results],
-            [],
-          ),
-          sortOrder.order,
-          'name',
-        )
-          .filter((person) => filterByName(person, search))
-          .filter((person) => filterByNationality(person, filter.nationality))
-          .filter((person) => filterByGender(person, filter.gender))}
-      />
-    </Table>
+    <React.Fragment>
+      <Table>
+        <TableBody
+          actions={{ view: viewAction }}
+          personList={orderPersonList(
+            //NOTE: probably expensive?
+            pages.reduce(
+              (store: Person[], current) => [...store, ...current.results],
+              [],
+            ),
+            sortOrder.order,
+            'name',
+          )
+            .filter((person) => filterByName(person, search))
+            .filter((person) => filterByNationality(person, filter.nationality))
+            .filter((person) => filterByGender(person, filter.gender))}
+        />
+      </Table>
+      {selectedPersonInfo ? (
+        <DisplayPerson
+          openState={[viewPerson, setViewPerson]}
+          person={selectedPersonInfo}
+        />
+      ) : null}
+    </React.Fragment>
   );
 };
 
