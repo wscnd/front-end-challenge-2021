4ec063ef32f477a1c597e38a777eac07e6ba83ba
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-21 11:06:16 -0300

    wip(pagination): fix components/Pagination to display pages index starting at 1

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -29,8 +29,8 @@ const PreviousOrNextPage: React.FunctionComponent<PreviousOrNextPageProps> = ({
   return (
     <button
       disabled={disabled}
-      onClick={onClick}
-      className="inline-flex items-center pt-4 pr-1 text-sm sm:prose font-semibold  border-t-2 border-transparent text-primary hover:text-gray-700 hover:border-primary disabled:border-transparent disabled:text-gray-200 disabled:cursor-not-allowed"
+      onClick={disabled ? () => void 0 : onClick}
+      className="inline-flex items-center pt-4 pr-1 text-sm sm:prose font-semibold  border-t-2 border-transparent text-primary hover:text-gray-700 hover:border-primary disabled:border-transparent disabled:text-gray-200 disabled:pointer-events-none"
     >
       <Icon className="w-5 h-5 mr-3 " aria-hidden="true" />
       {text}
@@ -51,12 +51,12 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({
   );
 
   React.useEffect(() => {
-    setNotHasNextPage(() => Number(currentPage) >= maxPages - 1 || isFetching);
+    setNotHasNextPage(() => Number(currentPage) >= maxPages || isFetching);
     setNotHasPreviousPage(
       () =>
         currentPage === null ||
-        Number(currentPage) === 0 ||
-        Number(currentPage) < 0 ||
+        Number(currentPage) === 1 ||
+        Number(currentPage) < 1 ||
         isFetching,
     );
   }, [currentPage, isFetching, maxPages]);
@@ -67,8 +67,9 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({
         <Link
           to={{
             pathname: '/',
-            search: `?page=${notHasPreviousPage ? 0 : Number(currentPage) - 1}`,
+            search: `?page=${Number(currentPage) - 1}`,
           }}
+          className={`${notHasPreviousPage ? 'pointer-events-none' : ''}`}
         >
           <PreviousOrNextPage
             onClick={actions.fetchPreviousPage}
@@ -82,22 +83,22 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({
       <div className="hidden md:-mt-px md:flex">
         {new Array(maxPages).fill(0).map((_, index) => {
           const pageStyle =
-            index === Number(currentPage)
+            index + 1 === Number(currentPage)
               ? 'inline-flex items-center px-4 pt-4 text-sm font-medium text-primary border-t-2 border-primary'
               : 'inline-flex items-center px-4 pt-4 text-sm font-medium text-gray-500 border-t-2 border-transparent hover:text-gray-700 hover:border-gray-300 ';
           return (
             <Link
               to={{
                 pathname: '/',
-                search: `?page=${Number(index)}`,
+                search: `?page=${Number(index + 1)}`,
               }}
               key={index}
             >
               <button
-                onClick={() => actions.fetchPage(index)}
+                onClick={() => actions.fetchPage(index + 1)}
                 className={pageStyle}
               >
-                {index}
+                {index + 1}
               </button>
             </Link>
           );
@@ -108,10 +109,9 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({
         <Link
           to={{
             pathname: '/',
-            search: `?page=${
-              notHasNextPage ? maxPages - 1 : Number(currentPage) + 1
-            }`,
+            search: `?page=${Number(currentPage) + 1}`,
           }}
+          className={`${notHasNextPage ? 'pointer-events-none' : ''}`}
         >
           <PreviousOrNextPage
             onClick={actions.fetchNextPage}
@@ -121,7 +121,6 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({
           />
         </Link>
       </div>
-
     </nav>
   );
 };
